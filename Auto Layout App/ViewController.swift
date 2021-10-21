8c247d620127038ead656057db810dc197dae4f6
Matthew Morgan <mmorganc@live.com>
2021-10-21 17:13:28 -0400

    Auto Layout App complete

--- Auto Layout App/ViewController.swift
+++ Auto Layout App/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer?
+    
+    @IBAction func clickMe(_ sender: Any) {
+        
+        playSound()
+    
+    
     }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
 
-}
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }}
 
