b793e0323536cc804f6fa5b852d774f495284137
Matthew Morgan <mmorganc@live.com>
2021-10-21 14:01:16 -0400

    Initial Commit

---
+++ Auto Layout App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout App
+//
+//  Created by Matthew  Morgan on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
